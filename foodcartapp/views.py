fc6e4c93a3a41e693ea3fb59adb8012afd1f3b05
IvanKMK <kumeyko@hotmail.com>
2021-10-05 23:53:06 +0200

    Send order data to frontend.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -24,7 +24,7 @@ class OrderItemSerializer(ModelSerializer):
 
 class OrderSerializer(ModelSerializer):
     products = OrderItemSerializer(
-        many=True, allow_empty=False
+        many=True, allow_empty=False, write_only=True
     )
 
     class Meta:
@@ -34,6 +34,7 @@ class OrderSerializer(ModelSerializer):
             'address', 'phonenumber',
             'products',
         ]
+
 
 def banners_list_api(request):
     # FIXME move data to db?
@@ -87,46 +88,10 @@ def product_list_api(request):
     })
 
 
-def validate(order):
-    errors = []
-    empty_keys = []
-
-    for key in order.keys():
-        if order[key] is None or order[key] == "":
-            empty_keys.append(key)
-    if empty_keys:
-        errors.append(
-            {', '.join(empty_keys): 'Это поле не может быть пустым.'})
-
-    if isinstance(order['firstname'], list):
-        errors.append({'firstname': ' Not a valid string.'})
-
-    if isinstance(order['products'], list) and len(order['products']) == 0:
-        errors.append({'products': 'Этот список не может быть пустым.'})
-
-    if isinstance(order['products'], str):
-        errors.append(
-            {'products': 'Ожидался list со значениями, но был получен "str".'})
-
-    for ordered_product in order['products']:
-        if not Product.objects.filter(id=ordered_product['product']):
-            errors.append({
-                'products': f'Недопустимый продукт {ordered_product["product"]}'
-            })
-
-    if not carrier._is_mobile(
-        number_type(phonenumbers.parse(order['phonenumber']))
-    ):
-        errors.append({'phonenumber': 'Введен некорректный номер телефона.'})
-
-    if errors:
-        raise ValidationError(errors)
-
-
 @api_view(['POST'])
 def register_order(request):
-
-    serializer = OrderSerializer(data=request.data)
+    order = request.data
+    serializer = OrderSerializer(data=order)
     serializer.is_valid(raise_exception=True)
 
     customer = Order.objects.create(
@@ -142,4 +107,7 @@ def register_order(request):
             product=Product.objects.get(id=product['product']),
             quantity=product['quantity']
         )
-    return Response(order)
+
+    serialized = OrderSerializer(order)
+    
+    return Response(serialized.data)
